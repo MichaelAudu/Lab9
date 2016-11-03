da0dd2a918f30c81f71530c099c93cf26c67df3d
MichaelAudu <michael.audu@aya.yale.edu>
2016-11-03 16:09:35 -0400

    First Commit

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -10,6 +10,88 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Circle Tester");
+
+            bool continueOn =true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter A Radius");
+                double radius;
+                bool isValid = double.TryParse(Console.ReadLine(), out radius);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid Entry");
+                }
+
+                else
+                {
+                    Circle circle = new Circle(radius);
+                    Console.WriteLine("Circumference: {0}",circle.getFormattedCircumference());
+                    Console.WriteLine("Area: {0}",circle.getFormattedArea());
+                    Console.WriteLine("Continue (Y/N?)");
+
+                    string goOn = Console.ReadLine();
+                    continueOn = goOn.ToLower() == "y" ? true : false;
+                }
+
+            } while (continueOn);
+
+            Console.WriteLine("Goodbye, You Created {0} Circles",Circle.NumberOfCircles);
+
         }
     }
+
+   
+
+    class Circle
+    {
+        public static int NumberOfCircles;
+
+        private double _radius;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        double Circumference;
+        double Area;
+
+
+        public Circle(double radius)
+
+
+        {
+            Circumference = 2 * Math.PI * radius;
+            Area = Math.PI * radius * radius;
+            NumberOfCircles += 1;
+
+        }
+
+            public double getCircumference()
+        {
+            return Circumference;
+        }
+
+        public string getFormattedCircumference()
+        {
+            return Circumference.ToString("0.00");
+        }
+
+        public double getArea()
+        {
+            return Area;    
+        }
+
+        public string getFormattedArea()
+        {
+            return Area.ToString("0.00");
+        }
+        }
+
+
 }
